617afa482748a0821e80179e6e1b89d64edfec9e
Andrey Potekhin <andrey1@taitl.com>
2019-04-02 11:59:12 -0700

    Verify docker swarm is available, as required by docker secrets

--- src/common.sh
+++ src/common.sh
@@ -4,20 +4,41 @@ command_exist() {
 
 require_docker() {
 	if ! command_exist docker; then
-	  echo 'Docker command not found, aborting' >&2
+      echo 'CHECKING IF DOCKER COMMAND IS AVAILABLE>' >&2
+	  echo 'Docker command not found, exiting.' >&2
 	  exit 1
 	fi
 	
 	if ! command_exist docker-compose; then
-	  echo 'Docker-compose command not found, aborting' >&2
+      echo 'CHECKING IF DOCKER COMPOSE COMMAND IS AVAILABLE>' >&2
+	  echo 'Docker-compose command not found, exiting.' >&2
+	  exit 1
+	fi
+}
+
+require_docker_swarm() {
+	if [ -z "`docker info | grep -F 'Swarm: active'`" ]; then
+      echo 'CHECKING IF DOCKER SWARM IS AVAILABLE>' >&2
+	  echo 'Docker swarm not found. Use "docker swarm init" (or "docker swarm join") to connect this node to swarm.' >&2
+	  exit 1
+	fi
+}
+
+require_this_node_in_docker_swarm() {
+	if ! docker node ls &> /dev/null; then
+      echo 'CHECKING WHETHER THIS NODE IS PART OF DOCKER SWARM>' >&2
+      docker node ls  >&2
+	  echo 'This node is not a swarm manager. Use "docker swarm init" (or "docker swarm join") to connect this node to swarm.' >&2
 	  exit 1
 	fi
 }
 
 startup_sequence(){
 	require_docker;
-	
+	require_docker_swarm;
+	require_this_node_in_docker_swarm;
 }
+
 common_init(){
 	startup_sequence;
 }
